ea59d1e9d0fa1e998757db919def9a8dc7d3c285
Jasper Vos <j.a.c.vos@student.tudelft.nl>
2025-05-19 21:52:15 +0200

    change buckets

--- app-service/Util/MetricsRegistry.cs
+++ app-service/Util/MetricsRegistry.cs
@@ -14,7 +14,11 @@ public static class MetricsRegistry
         Metrics.CreateCounter("prediction_count", "Total number of submits");
 
     public static readonly Histogram PredictionResponseTime =
-        Metrics.CreateHistogram("prediction_response_times", "Backend prediction response time in seconds");
+        Metrics.CreateHistogram("prediction_response_times", "Backend prediction response time in seconds", new HistogramConfiguration
+        {
+            Buckets = Histogram.LinearBuckets(start: 0, width: .05, count: 20)
+        }
+        );
 
     public static void UpdateIncorrectGauge()
     {
